68f58e6a080cda5b0e2321aad0520fd15852664f
Shagufta-Naz-123 <shagufta590051@gmail.com>
2024-11-29 23:30:28 -0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,19 @@
+// app/page.js ya pages/index.js
+
+
+import About from "./components/header/about/page";
+import Header from "./components/header/page";
+
+
+
+export default function Home() {
+    return (
+        <main>
+            <Header />
+            <About />
+            
+        
+            
+        </main>
+    );
+}
